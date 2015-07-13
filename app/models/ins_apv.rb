52f09d29ce8a4c2bc614c0a3d5e86c23726a265f
claudiaguzman <claudia.guzmans@usach.cl>
2015-07-13 14:34:44 -0300

    cambio vista de instancias

--- app/models/ins_apv.rb
+++ app/models/ins_apv.rb
@@ -1,6 +1,6 @@
 class InsApv < ActiveRecord::Base
 	self.primary_key = :ins_apvs_cod
-	belongs_to :ins_cod, class_name: 'Instanci', foreign_key: 'ins_cod'
-	belongs_to :ins_cod_prov, class_name: 'Instanci', foreign_key: 'ins_cod_prov'
+	belongs_to :instanci, class_name: 'Instanci', foreign_key: 'ins_cod'
+	belongs_to :instanci, class_name: 'Instanci', foreign_key: 'ins_cod_prov'
 	belongs_to :art_prop_val, foreign_key: 'apv_cod'
 end
