be9b69f190923833bea18fbae3ce795c95d46262
claudiaguzman <claudia.guzmans@usach.cl>
2015-07-14 15:09:58 -0300

    camibio modelo instancia

--- app/models/instanci.rb
+++ app/models/instanci.rb
@@ -7,13 +7,13 @@ class Instanci < ActiveRecord::Base
 	has_many :detalle_sol_devolucions, foreign_key: 'ins_cod'
 	has_many :imagens, :dependent => :destroy, foreign_key: 'ins_cod'
 	has_many :ins_apvs, :dependent => :destroy, foreign_key: 'ins_cod' 
-	has_many :detalle_pedidos, foreign_key: 'ins_cod_prov'
-	has_many :detalle_documento_de_compras, foreign_key: 'ins_cod_prov' 
-	has_many :detalle_sol_devolucions, foreign_key: 'ins_cod_prov'
-	has_many :imagens, :dependent => :destroy, foreign_key: 'ins_cod_prov'
-	has_many :ins_apvs, :dependent => :destroy, foreign_key: 'ins_cod_prov'
+	# has_many :detalle_pedidos, foreign_key: 'ins_cod_prov'
+	# has_many :detalle_documento_de_compras, foreign_key: 'ins_cod_prov' 
+	# has_many :detalle_sol_devolucions, foreign_key: 'ins_cod_prov'
+	# has_many :imagens, :dependent => :destroy, foreign_key: 'ins_cod_prov'
+	# has_many :ins_apvs, :dependent => :destroy, foreign_key: 'ins_cod_prov'
 	accepts_nested_attributes_for :articulo, :reject_if => :all_blank, :allow_destroy => true
-	accepts_nested_attributes_for :ins_apvs, :reject_if => :all_blank, :allow_destroy => true
+	accepts_nested_attributes_for :imagens, :reject_if => :all_blank, :allow_destroy => true
 	validates :est_art_cod, presence: true
 	validates :ins_cod_prov, presence: true
 end
