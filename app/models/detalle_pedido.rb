dcf6a959f0d166faa027365a9f379aa02ee480ad
CamilaMarin <camila.marin@usach.cl>
2015-07-15 19:43:31 -0400

    cambios algunas vistas

--- app/models/detalle_pedido.rb
+++ app/models/detalle_pedido.rb
@@ -2,4 +2,8 @@ class DetallePedido < ActiveRecord::Base
 	self.primary_keys = :ped_cod, :det_ped_linea
 	belongs_to :instanci, foreign_key: 'ins_cod'
 	belongs_to :pedido, foreign_key: 'ped_cod'
+	validates :ped_cod, presence: {message: "es requerido"}
+	validates :det_ped_linea, presence: {message: "es requerido"}
+	validates :ins_cod, presence: {message: "es requerido"}
+	validates :ins_cod_prov, presence: {message: "es requerido"}
 end
