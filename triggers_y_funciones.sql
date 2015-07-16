3f27ef4dead92d9ba35c13cbfee2d4f395604cea
CamilaMarin <camila.marin@usach.cl>
2015-07-16 04:24:59 -0400

    backup y transicion estado pedido

---
+++ triggers_y_funciones.sql
@@ -0,0 +1,34 @@
+﻿CREATE OR REPLACE FUNCTION estado_dc_history() returns TRIGGER AS $$
+BEGIN
+  INSERT INTO historial_estado_dcs(doc_com_cod,est_dc_cod,hist_est_dc_cod,hist_est_dc_fecha)
+  VALUES(OLD.doc_com_cod
+    ,OLD.est_dc_cod
+    ,NEW.est_dc_cod
+    ,CURRENT_TIMESTAMP);
+  RETURN null;
+END;
+$$ 
+language 'plpgsql';
+
+CREATE TRIGGER tgr_hist_dc 
+AFTER UPDATE OF est_dc_cod
+ON documento_de_compras 
+FOR EACH ROW
+  EXECUTE PROCEDURE estado_dc_history();
+
+CREATE OR REPLACE FUNCTION estado_ped_history() returns TRIGGER AS $$
+BEGIN
+  INSERT INTO historial_estado_pedidos(ped_cod,estado_ped_cod,hist_est_ped_cod,hist_est_ped_fecha)
+  VALUES(OLD.ped_cod,OLD.estado_ped_cod
+    ,NEW.estado_ped_cod
+    ,CURRENT_TIMESTAMP);
+  RETURN null;
+END;
+$$ 
+language 'plpgsql';
+
+CREATE TRIGGER tgr_hist_ped
+AFTER UPDATE OF estado_ped_cod
+ON pedidos
+FOR EACH ROW
+  EXECUTE PROCEDURE estado_ped_history();
